46cb68033c961867ce0a9a6124e87d250e74e181
Kiwon Kim <kiwon@elicer.com>
2023-04-13 02:12:25 +0900

    feat: add amount increment dispatch function

--- src/pages/redux/index.tsx
+++ src/pages/redux/index.tsx
@@ -1,30 +1,60 @@
-import { decrement, increment } from '../../redux/counterSlice';
+import { useState } from 'react';
+
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from '../../redux/counterSlice';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 
 const ReduxPage = () => {
   const count = useAppSelector((state) => state.counter.value);
   const dispatch = useAppDispatch();
 
+  const [value, setValue] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
   return (
-    <>
-      <div>
-        <button
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          Increment
-        </button>
-      </div>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '1rem',
+      }}
+    >
       <div>{count}</div>
       <div>
         <button
-          aria-label="Decrement value"
+          onClick={() => dispatch(increment())}
+          style={{ padding: '1rem' }}
+        >
+          Increment
+        </button>
+        <button
           onClick={() => dispatch(decrement())}
+          style={{ padding: '1rem' }}
         >
           Decrement
         </button>
       </div>
-    </>
+      <div>
+        <input
+          type="number"
+          value={value}
+          onChange={handleChange}
+          style={{ padding: '1rem' }}
+        />
+        <button
+          onClick={() => dispatch(incrementByAmount(Number(value)))}
+          style={{ padding: '1rem' }}
+        >
+          increment By Amount
+        </button>
+      </div>
+    </div>
   );
 };
 
